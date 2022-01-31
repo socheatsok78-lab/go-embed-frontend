2084e42084f9bc31f3b9d609430ba152201ee7a4
Socheat Sok <alex@socheat.net>
2022-01-31 21:14:18 +0700

    feat: update example project

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/soramitsukhmer/ncd-frontend
+module github.com/socheatsok78-lab/go-embed-frontend
 
 go 1.17
+
+require github.com/socheatsok78-lab/dedebme v0.0.0-20220131140823-06721c515e42 // indirect
--- main.go
+++ main.go
@@ -5,93 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"path/filepath"
-	"strings"
 
-	"github.com/soramitsukhmer/ncd-frontend/dist"
+	"github.com/socheatsok78-lab/dedebme"
+	"github.com/socheatsok78-lab/go-embed-frontend/dist"
 )
-
-var mimeTypes = map[string]string{
-	".js":  "application/javascript",
-	".css": "text/css",
-}
-
-// See https://stackoverflow.com/questions/26141953/custom-404-with-gorilla-mux-and-std-http-fileserver
-func intercept404(handler, on404 http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		hookedWriter := &hookedResponseWriter{ResponseWriter: w}
-
-		ext := filepath.Ext(r.URL.Path)
-		mime := mimeTypes[ext]
-
-		if mime != "" {
-			w.Header().Set("Content-Type", mime+"; charset=utf-8")
-		}
-
-		handler.ServeHTTP(hookedWriter, r)
-
-		if hookedWriter.got404 {
-			on404.ServeHTTP(w, r)
-		}
-
-		fmt.Println("==> Request:", r.URL.Path, mime)
-	})
-}
-
-type hookedResponseWriter struct {
-	http.ResponseWriter
-	got404 bool
-}
-
-func (hrw *hookedResponseWriter) WriteHeader(status int) {
-	if status == http.StatusNotFound {
-		// Don't actually write the 404 header, just set a flag.
-		hrw.got404 = true
-	} else {
-		hrw.ResponseWriter.WriteHeader(status)
-	}
-}
-
-func (hrw *hookedResponseWriter) Write(p []byte) (int, error) {
-	if hrw.got404 {
-		// No-op, but pretend that we wrote len(p) bytes to the writer.
-		return len(p), nil
-	}
-
-	return hrw.ResponseWriter.Write(p)
-}
-
-func serveFileContents(file string, files http.FileSystem) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// Restrict only to instances where the browser is looking for an HTML file
-		if !strings.Contains(r.Header.Get("Accept"), "text/html") {
-			w.WriteHeader(http.StatusNotFound)
-			fmt.Fprint(w, "404 not found")
-
-			return
-		}
-
-		// Open the file and return its contents using http.ServeContent
-		index, err := files.Open(file)
-		if err != nil {
-			w.WriteHeader(http.StatusNotFound)
-			fmt.Fprintf(w, "%s not found", file)
-
-			return
-		}
-
-		fi, err := index.Stat()
-		if err != nil {
-			w.WriteHeader(http.StatusNotFound)
-			fmt.Fprintf(w, "%s not found", file)
-
-			return
-		}
-
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		http.ServeContent(w, r, fi.Name(), fi.ModTime(), index)
-	}
-}
 
 func getFrontendAssets() http.FileSystem {
 	return http.FS(dist.Assets)
@@ -103,10 +20,9 @@ func main() {
 	flag.Parse()
 
 	httpFS := getFrontendAssets()
-	fileServer := http.FileServer(httpFS)
-	serveIndex := serveFileContents("index.html", httpFS)
+	handler := dedebme.Server(httpFS)
 
-	http.Handle("/", intercept404(fileServer, serveIndex))
+	http.Handle("/", handler)
 
 	addr := fmt.Sprintf("localhost:%d", port)
 	fmt.Printf("Serving app at http://%s\n", addr)
